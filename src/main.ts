058578cc835511881509208af4d8a7eb43b965e7
Smurfstuffy <markoyanovych123@gmail.com>
2026-02-26 18:44:06 +0200

    feat(swagger): add bearer auth to swagger

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Train Schedule API')
     .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
